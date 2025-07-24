2819236e8d967c594fbac2dff5950d9dbca26a00
Ana Balica <ana.balica@gmail.com>
2025-07-24 12:20:13 +0100

    Update music_for_paul.py

--- music_for_paul.py
+++ music_for_paul.py
@@ -2,20 +2,21 @@ import pygame
 from gpiozero import Button
 from signal import pause
 
-button_song_map = {
-  23: '/home/banana/song1.wav"
-}
-
-blue_button = Button(23)
-print("button init")
-
 pygame.mixer.pre_init(buffer=4096)
 pygame.mixer.init()
+print("mixer init")
 
-def play_sound(filepath):
-  sound = pygame.mixer.Sound(filepath)
-  playing = sound.play()
-  while playing.get_busy():
-    pygame.time.delay(100)
+button_song_map = {
+  Button(23): '/home/banana/song1.wav',
+  Button(24): '/home/banana/song2.wav',
+}
+print("button init")
 
-blue_button.when_pressed = lambda: play_sound('/home/banana/song1.wav")
+def play_sound(button, song):
+  pygame.mixer.stop()
+  song.play()
+
+for button, song in button_song_map.items():
+  button.when_pressed = lambda button: play_sound(button, song)
+
+pause()
