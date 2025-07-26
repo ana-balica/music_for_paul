4f8e85f6f2e5943aba4bdb4a089444fcab771d79
Ana Balica <ana.balica@gmail.com>
2025-07-26 12:56:42 +0100

    Update music_for_paul.py

--- music_for_paul.py
+++ music_for_paul.py
@@ -7,12 +7,12 @@ pygame.mixer.init()
 print("mixer init")
 
 button_song_map = {
-  Button(18): pygame.mixer.Sound('/home/banana/abba_mamma_mia.mp3'),
-  Button(23): pygame.mixer.Sound('/home/banana/bee_gees_stayin_alive.mp3'),
-  Button(24): pygame.mixer.Sound('/home/banana/christina_perri_you_are_my_sunshine.mp3'),
-  Button(25): pygame.mixer.Sound('/home/banana/louis_armstrong_what_a_wonderful_world.mp3'),
-  Button(12): pygame.mixer.Sound('/home/banana/nutcracker.mp3'),
-  Button(16): pygame.mixer.Sound('/home/banana/the_beatles_here_comes_the_sun.mp3'),
+  Button(18): pygame.mixer.Sound('/home/banana/Music/abba_mamma_mia.mp3'),
+  Button(23): pygame.mixer.Sound('/home/banana/Music/bee_gees_stayin_alive.mp3'),
+  Button(24): pygame.mixer.Sound('/home/banana/Music/christina_perri_you_are_my_sunshine.mp3'),
+  Button(25): pygame.mixer.Sound('/home/banana/Music/louis_armstrong_what_a_wonderful_world.mp3'),
+  Button(12): pygame.mixer.Sound('/home/banana/Music/nutcracker.mp3'),
+  Button(16): pygame.mixer.Sound('/home/banana/Music/the_beatles_here_comes_the_sun.mp3'),
 }
 stop_button = Button(4)
 print("buttons init")
