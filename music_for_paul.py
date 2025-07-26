414adbcc11528a0faaa6bcfc4dcf7e3d5fbd0952
Ana Balica <ana.balica@gmail.com>
2025-07-26 12:54:59 +0100

    Update music_for_paul.py

--- music_for_paul.py
+++ music_for_paul.py
@@ -7,15 +7,21 @@ pygame.mixer.init()
 print("mixer init")
 
 button_song_map = {
-  Button(23): pygame.mixer.Sound('/home/banana/song1.wav'),
-  Button(24): pygame.mixer.Sound('/home/banana/song2.wav'),
+  Button(18): pygame.mixer.Sound('/home/banana/abba_mamma_mia.mp3'),
+  Button(23): pygame.mixer.Sound('/home/banana/bee_gees_stayin_alive.mp3'),
+  Button(24): pygame.mixer.Sound('/home/banana/christina_perri_you_are_my_sunshine.mp3'),
+  Button(25): pygame.mixer.Sound('/home/banana/louis_armstrong_what_a_wonderful_world.mp3'),
+  Button(12): pygame.mixer.Sound('/home/banana/nutcracker.mp3'),
+  Button(16): pygame.mixer.Sound('/home/banana/the_beatles_here_comes_the_sun.mp3'),
 }
-print("button init")
+stop_button = Button(4)
+print("buttons init")
 
 def play_sound(button, song):
   pygame.mixer.stop()
   song.play()
 
+stop_button.when_pressed = pygame.mixer.stop
 for button, song in button_song_map.items():
   button.when_pressed = lambda button: play_sound(button, song)
 
